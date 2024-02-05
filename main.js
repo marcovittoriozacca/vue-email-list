77e09a2361c9eb85865491ee68819874a12e7d1b
marcovittoriozacca <zaccamarco01@gmail.com>
2024-02-05 16:57:07 +0100

    feat: 10 emails generated from API

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ const { createApp } = Vue
 createApp({
 data() {
     return {
-    message: 'Hello Vue!'
+        url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+        emails: [],
     }
-}
+},
+methods: {
+    randomEmail(){
+        this.emails = []
+        for (let i = 0; i < 10; i++) {
+            axios.get(this.url)
+            .then( (value) => this.emails.push(value.data.response))  
+        }
+
+    }
+},
+
 }).mount('#app')
